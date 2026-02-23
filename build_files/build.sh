a41e49cadb0a21ae4a99f228f85353177edd1faa
19kms <1929something2@gmail.com>
2026-02-23 18:28:58 +0000

    Initial commit: Add snapd, waydroid, and remove dnf restrictions

---
+++ build_files/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# Install snapd for snap package support
+dnf5 install -y snapd
+
+# Create snapd socket directory (required for snap to function properly)
+mkdir -p /var/lib/snapd
+
+# Install and initialize waydroid (Android container)
+dnf5 install -y waydroid
+# Enable waydroid service
+systemctl enable waydroid-container
+
+# Remove the "short leash" restrictions to allow unrestricted dnf/package installation
+# This removes polkit rules that prevent interactive package installation
+rm -f /etc/polkit-1/rules.d/*package* /etc/polkit-1/rules.d/*rpm*
+
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+
+systemctl enable snapd.socket
+systemctl enable snapd.apparmor.service
+systemctl enable podman.socket
